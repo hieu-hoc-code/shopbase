feecb67af96fe4aa136333de1e7930b1e385e46c
minhchinhctk41 <minhchinh04@gmail.com>
2021-09-11 18:18:17 +0700

    Design Cart

--- client/src/views/cart/Cart.vue
+++ client/src/views/cart/Cart.vue
@@ -1,56 +1,93 @@
 <template>
-  <div class="container">
-    <h2>Giỏ hàng ({{ getCart.quantity }})</h2>
-    <div class="title">
-      <span>mua</span>
-      <span>hình ảnh</span>
-      <span>tên sp</span>
-      <span>Đơn giá</span>
-      <span>số lượng</span>
-      <span>thành tiền</span>
-      <span>xóa</span>
-    </div>
-    <div v-for="cart in getCart.cart" :key="cart.id" class="product">
-      <input
-        type="checkbox"
-        name="choose"
-        v-model="ordered"
-        :value="{ cart }"
-        @change="checkboxHandler"
-      />
-      <img src="" alt="Hinh anh" />
-      <span>{{ cart.name }}</span>
-      <span>{{ cart.price }}</span>
-      <div>
-        <button @click="sub_quantity(cart.id, cart.quantity)">-</button>
-        <span>{{ cart.quantity }}</span>
-        <button @click="add_quantity(cart.product_id)">+</button>
+  <div class="main">
+    <div class="container">
+      <div class="product-content">
+        <div class="cart">
+          <h2>Giỏ hàng ({{ getCart.quantity }})</h2>
+        </div>
+        <div class="product">
+          <table>
+            <tr class="title">
+              <td>Mua</td>
+              <td class="img">Hình ảnh</td>
+              <td>Tên SP</td>
+              <td>Đơn giá</td>
+              <td>Số lượng</td>
+              <td>thành tiền</td>
+              <td>Xóa</td>
+            </tr>
+            <tr v-for="cart in getCart.cart" :key="cart.id" class="product">
+              <td>
+                <input
+                  type="checkbox"
+                  name="choose"
+                  v-model="ordered"
+                  :value="{ cart }"
+                  @change="checkboxHandler"
+                />
+              </td>
+              <td class="img"><img :src="sp1" alt="Hinh anh" /></td>
+              <td>{{ cart.name }}</td>
+              <td>{{ cart.price }}</td>
+              <td>
+                <i class="fa fa-caret-left" @click="sub_quantity(cart.id, cart.quantity)"></i>
+                <span>{{ cart.quantity }}</span>
+                <i class="fa fa-caret-right" @click="add_quantity(cart.product_id)"></i>
+              </td>
+              <td>{{ cart.quantity * cart.price }}</td>
+              <td><button @click="remove_quantity(cart.id, cart.quantity)">xóa</button></td>
+            </tr>
+          </table>
+        </div>
       </div>
-      <span>{{ cart.quantity * cart.price }}</span>
-      <button @click="remove_quantity(cart.id, cart.quantity)">xóa</button>
-    </div>
-    <div>
-      <div class="tamtinh">
-        <span>Tạm tính</span>
-        <span>{{ getOrder }}đ</span>
+      <div class="price">
+        <div class="pro">
+          <div class="km">
+            <span>Khuyến mãi</span>
+            <router-link to="#">
+            <i class="fa fa-pencil-square-o"></i>
+            Nhập khuyến mãi
+            </router-link>
+          </div>
+
+        </div>
+        <div class="bill">
+          <div class="bill-content">
+            <table>
+              <tr>
+                <td>Tạm tính :</td>
+                <td>{{ getOrder }}đ</td>
+              </tr>
+              <tr>
+                <td>Giảm giá :</td>
+                <td>0đ</td>
+              </tr>
+              <tr class="sum">
+                <td>Tổng cộng :</td>
+                <td>{{ getOrder }}đ</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div class="order">
+          <button @click="orderHandler">Đặt hàng</button>
+        </div>
+
       </div>
-      <div class="giamgia">
-        <span>Giảm giá</span>
-        <span>0đ</span>
-      </div>
-      <button @click="orderHandler">Đặt hàng</button>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import sp1 from './../../assets/product/sp1.jpg'
 
 export default {
   name: 'Cart',
   data() {
     return {
       ordered: [],
+      sp1
     }
   },
   computed: {
@@ -79,7 +116,6 @@ export default {
     remove_quantity(cart_id, quantity) {
       let payload = { cart_id: cart_id, amount: quantity }
       this.removeCart(payload)
-
     },
     checkboxHandler() {
       this.updateOrder(this.ordered)
@@ -91,6 +127,5 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-  @import './cart.scss';
-
+@import './cart.scss';
 </style>
